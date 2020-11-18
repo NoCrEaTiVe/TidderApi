c556e033ac87e116cca3c9e8a950b97d6f7eaefd
Boris Ignatov <boris.ignatov.99@gmail.com>
2020-11-18 15:22:11 +0600

    all endpoints

--- tidder/core/serializers.py
+++ tidder/core/serializers.py
@@ -5,5 +5,29 @@ from .models import *
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
-        fields = ('id','user','title','community','text','rating','image')
+        fields = ['id','user','community','title','text','rating','image']
+        read_only_fields = ('id',)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ('id','user','text','post','rating')
+        read_only_fields = ('id',)
+
+
+class PostDetailSerializer(serializers.ModelSerializer):
+    comment = CommentSerializer()
+    class Meta:
+        model = Post
+        fields = ['id','user','community','title','text','rating','image']
+        read_only_fields = ('id',)
+        depth = 1
+
+
+class CommunitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Community
+        fields = ('id','name','description')
         read_only_fields = ('id',)
--- tidder/core/views.py
+++ tidder/core/views.py
@@ -1,13 +1,133 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from core import serializers
+from rest_framework.views import APIView
+from django.shortcuts import get_object_or_404
 from rest_framework import generics, authentication, permissions, viewsets, mixins, status
 from .models import *
 
 
-class PostViewSet(viewsets.GenericViewSet,mixins.ListModelMixin,mixins.CreateModelMixin,mixins.UpdateModelMixin):
+class PostView(viewsets.GenericViewSet,mixins.ListModelMixin,mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,mixins.DestroyModelMixin):
     serializer_class = serializers.PostSerializer
     queryset = Post.objects.all()
-
     def get_queryset(self):
         return self.queryset
+
+
+class PostDetailView(APIView):
+    def get_object(self, id):
+        try:
+            return Post.objects.get(pk=id)
+        except Post.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id):
+        post = self.get_object(id)
+        serializer = serializers.PostSerializer(post)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        post = self.get_object(id)
+        serializer = serializers.PostSerializer(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        post = self.get_object(id)
+        post.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+class CommentView(APIView):
+    def get(self, request, id):
+        comments = Comment.objects.filter(post=id)
+
+        if 'order_by' in request.query_params:
+            comments = comments.order_by(request.query_params['order_by'])
+
+        serializer = serializers.CommentSerializer(comments, many=True)
+        return Response(serializer.data)
+
+
+class CommentDetailView(APIView):
+    def get_object(self, id):
+        try:
+            return serializers.Comment.objects.get(pk=id)
+        except Comment.DoesNotExist:
+            raise Http404
+
+    def get(self, request, id):
+        comment = self.get_object(id)
+        serializer = serializers.CommentSerializer(comment)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        comment = self.get_object(id)
+        serializer = serializers.CommentSerializer(comment, data=request.data, partial=True)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        comment = self.get_object(id)
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CommentList(viewsets.GenericViewSet,mixins.ListModelMixin,mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+    serializer_class = serializers.CommentSerializer
+    queryset = Comment.objects.all()
+    def get_queryset(self):
+        return self.queryset
+
+
+class CommunityView(viewsets.GenericViewSet,mixins.ListModelMixin,mixins.CreateModelMixin,
+                    mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+    serializer_class = serializers.CommunitySerializer
+    queryset = Community.objects.all()
+    def get_queryset(self):
+        return self.queryset
+
+
+class CommunityDetailView(APIView):
+    def get_object(self, id):
+            try:
+                return Community.objects.get(pk=id)
+            except Community.DoesNotExist:
+                raise Http404
+
+    def get(self, request, id):
+        community = self.get_object(id)
+        serializer = serializers.CommunitySerializer(subreddit)
+        return Response(serializer.data)
+
+    def put(self, request, id):
+        community = self.get_object(id)
+        serializer = serializers.SubredditSerializer(
+            community, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, id):
+        community = self.get_object(id)
+        community.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class PostsByCommunity(APIView):
+    def get(self, request, id):
+        posts = Post.objects.filter(community=id)
+        if 'order_by' in request.query_params:
+            posts = posts.order_by(request.query_params['order_by'])
+        serializer = serializers.PostSerializer(posts, many=True)
+        return Response(serializer.data)
